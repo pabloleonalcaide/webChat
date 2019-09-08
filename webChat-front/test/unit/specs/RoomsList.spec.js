2a1abfeea008af422e5b8947b9f18889979f22d1
pabloleonalcaide <pabloleonpsico@gmail.com>
2019-09-08 17:48:57 +0200

    New test for RoomsList and modified gitIgnore file

--- webChat-front/test/unit/specs/RoomsList.spec.js
+++ webChat-front/test/unit/specs/RoomsList.spec.js
@@ -1,5 +1,24 @@
-describe('RoomList component', () => {
-  it('should render the component correctly', () => {
-    expect(null).toBeNull()
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+import Roomslist from '@/components/Roomslist'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('RoomsList Component', () => {
+  let store
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {},
+      getters: {
+        currentUser: jest.fn()
+      }
+    })
+  })
+  it('should render correct the List of rooms', () => {
+    const component = shallowMount(Roomslist, {store, localVue})
+    expect(component.findAll('.roomsList').length).toBe(1)
   })
 })
